07f0522e63aff8d59e36e560bbf22255f3c8b2f2
simorighi <simone.righi@marconirovereto.it>
2026-02-03 21:01:14 +0100

    lats commit

--- SPAEsameWeb/src/utils/weatherApi.ts
+++ SPAEsameWeb/src/utils/weatherApi.ts
@@ -1,7 +1,7 @@
 const API_KEY = "01eba156d98ea1360e50576692a3a6e6";
 /*
 chiave api secondaria qual'ora la prima scadesse
-const API_KEY = "" */
+const API_KEY = "50c17af06b6fbf49e6c694d9d4c8c8e1" */
 
 const BASE_URL = "https://api.openweathermap.org";
 
